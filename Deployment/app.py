f93f1b9c3fbb1c9a5c5f6ff51ee25cef67861119
AB <12748752+iAmKankan@users.noreply.github.com>
2022-02-10 17:26:04 +0530

    Add files via upload

--- Deployment/app.py
+++ Deployment/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
-app = Flask(__name__) # referencing this file
+app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def index():
-    return "Hello world"
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
+api.add_resource(HelloWorld, '/')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
